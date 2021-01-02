92599d6a40a130204ef51b7961bbec23f49641c8
Md Fuadul Islam Redoy <mdfuadulislamhridoy52@gmail.com>
2021-01-02 14:27:22 +0600

    Frontend complete and ready to use database

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="(todo,id) in todos" :key="id">
+      <TodoItem v-bind:todo="todo"
+                v-on:del-todo="$emit('del-todo', todo.id)"
+      />
+      <!--    first todo is props name and second is data-->
+
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem";
+
+export default {
+  props: ["todos"],
+  components: {
+    TodoItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
